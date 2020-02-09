42d12b00cb8ad8e78c38335060f3dc575576ecaa
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-02-09 11:27:29 -0300

    Criação da tabela delivermen. Criação da configuração do sequelize

---
+++ src/database/migrations/20200209141831-create-table-deliverman.js
@@ -0,0 +1,37 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('delivermen', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      avatar_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('delivermen');
+  },
+};
