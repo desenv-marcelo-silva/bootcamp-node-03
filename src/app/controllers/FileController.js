b84ef0d0c1dbd2083dfdd8dbd4a959960b128229
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-02-09 12:01:28 -0300

    FilesController: implementação de upload do arquivo.

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -1,7 +1,15 @@
+import File from '../models/File';
+
 class FileController {
   async store(req, res) {
-      return res.json({ok: 'FileController'});
+      const { originalname: name, filename: path } = req.file;
+  
+      const file = await File.create({
+        name,
+        path,
+      });
+  
+      return res.json(file);
   }
 }
-
 export default new FileController();
